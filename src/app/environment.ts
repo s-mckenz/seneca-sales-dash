30b284121a34d6cffafe1eb49d1fb288d9a63336
s-mckenz <125470631+s-mckenz@users.noreply.github.com>
2025-03-26 13:41:46 -0400

    update user pool & app client

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -9,14 +9,14 @@ export const environment = {
       loginWith: {
         oauth: {
           redirectSignIn: [
-            "http://localhost:4200/dashboard",
-            "https://senecapayroll.s4hospitality.net/dashboard",
+            "http://localhost:4200/",
+            "https://senecapayroll.s4hospitality.net/",
           ],
           redirectSignOut: [
             "http://localhost:4200/",
             "https://senecapayroll.s4hospitality.net/",
           ],
-          domain: 'us-east-10whypnnrn.auth.us-east-1.amazoncognito.com',
+          domain: 'https://us-east-19do881wux.auth.us-east-1.amazoncognito.com',
           
           scope: ['openid', 'email', 'profile', 'aws.cognito.signin.user.admin'], 
           clientId: '34ibbsvkc014g5q7r5nc3pm8p5',
@@ -29,6 +29,6 @@ export const environment = {
   graphqlUri: 'https://rds-cloud-staging.hasura.app/v1/graphql',
   authorizationToken: 'tAOkaxydbwpbuaqYnmApQl6d6WDxHxFKehW1RNx3ZTcUCMfsFxb7HeJ46G1lKY2b',
   googleClientId:'61605134382-5h228ll6cotfj1tojnitvn86op7qjo9r.apps.googleusercontent.com',
-  cognitoUrl: 'https://0whypnnrn.auth.us-east-1.amazoncognito.com', // Fixed
-  cognitoAuthority: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_0WHyPNNRN', // Fixed
+  cognitoUrl: 'https://9do881wux.auth.us-east-1.amazoncognito.com', // Fixed
+  cognitoAuthority: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_9DO881wuX', // Fixed
 };
